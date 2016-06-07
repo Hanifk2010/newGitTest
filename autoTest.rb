6273e7ef369f00ff2bf7d28247e4b123cc1d622a
Hanifk2010 <khanif2010@gmail.com>
2016-06-07 11:04:07 -0500

    changes to auto file

--- autoTest.rb
+++ autoTest.rb
@@ -1,6 +1,7 @@
 require 'rspec'
 require 'watir-webdriver'
 
-@browser=Watir::Browser.new :chrome , :switches => %w[--disable-popup-blocking]
+ @browser=Watir::Browser.new :chrome , :switches => %w[--disable-popup-blocking]
 
  @browser.goto('google.com')
+ @browser.text_field(:title => 'Search').set ('phytel')
